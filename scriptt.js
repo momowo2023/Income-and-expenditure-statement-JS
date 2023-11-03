b7240a94d27c270011a5da1f34d0d395aab55e64
momowei <megwu2020@gmail.com>
2023-11-03 15:56:03 +0100

    steps 1-4 done

--- scriptt.js
+++ scriptt.js
@@ -13,53 +13,65 @@
 //1. doing the name property as the astring  ok
 //2. create the manu and different choice for the user ok
 //3. take the input from the user, store the input in a variable ok
-//4. use the stored to show the menu choice
+//4. use the stored to show the menu choice ok
 
 1
 //object 
 const account = { 
    name: "Mowei", //1. doing the name property as the astring
-   expenses:"",
-   income:"",
-   addExpenses:"",
-   addIncome: "",
-   ListAllExpenses:"",
-   getSummary:"",
+   expenses:[],
+   income:[],
+   addExpenses: function (item, amount){
+    this.expenses.push(item, amount); //use push to add a array
+   },
+   addIncome: function (item, amount){
+    this.income.push(item, amount); 
+   },
+   ListAllExpenses:[],
+   getSummary:[],
 };
 
 
 
- // the function menu() should only be responsible to show the different choices
-// for the user.
+
 //function
 function menu(){ 
     const choice = parseFloat( //use parseFloat to make the input as a number instead of string
         prompt(
-         "Please select from the menu:\n1. Add Income\n2. Add Expenses\n3. List of all Expenses\n4. Check out the balance"
+         "Please select from the menu:\n1. Add Income\n2. Add Expenses\n3. List of all Expenses\n4. Check out the balance\n5. I am good "
         )
     );//2. create the manu and different choice for the user
 
     switch (choice){ // use the stored to show the menu choice
         case 1:
+            const incomeItem = prompt("Enter the item of income");
             const income = parseFloat(prompt("Enter income amount:"));
             // make income to be added up；
+            menu();
             break;
         case 2: 
+            const expensesItem = prompt("Enter the item of expenses");
             const expenses = parseFloat(prompt("Enter expenses amount:"));
             // make it to be added up；
+            menu();
             break;
         case 3: 
             // to be solved
-            
+            menu();
             break;
         case 4: 
             // to be solved
-            
+            menu();
             break;
-
-    }
-    console.log( choice ? `Let's check No. ${choice}` : "Please select from the menu" );
-    //3. take the input from the user, store the input in a variable, and fix null
-}
+        case 5: 
+            const finalChoice = alert("See ya!")
+            break;
+        default:
+            const defaultChoice = alert("Please select from the menu");    
+            menu();                                                        
+            break;   
+        }
+    console.log(`let's check number ${choice}`);    //3. take the input from the user, store the input in a variable, and fix null
+}                                                   //4. use the stored to show the menu choice
 
 menu();
